99e6c85eb755a8bf37f6cb269ee068b10cac5005
Richard Poulson <contact@richardpoulson.com>
2019-08-06 16:23:17 -0600

    Work History view almost done

--- vuecli-vuetify-cd/src/views/EducationHistory.vue
+++ vuecli-vuetify-cd/src/views/EducationHistory.vue
@@ -5,15 +5,18 @@
         <span class="display-2">Education History</span>
       </v-layout>
     </v-container>
-    <v-container fluid grid-list-md text-center>
+    <v-container fluid>
       <v-layout wrap align-end justify-center row fill-height>
-        <v-flex grow-shrink-0 xs11 sm5 md3 ma-2 pa-2 v-for="(item, i) in history" :key="i">
-          <v-card dark color="primary">
-            {{ item.name }}
-            <br />
-            {{ item.degree }}
-            <br />
-            {{ item.gpa }}
+        <v-flex xs10 sm5 md3 ma-2 pa-2 v-for="(item, i) in history" :key="i">
+          <v-card class="pa-2" hover>
+            <v-layout align-center justify-center>
+              <p class="title text-center">{{ item.name }}</p>
+            </v-layout>
+            <v-divider></v-divider>
+            <ul class="subtitle-2 pl-2">
+              <li>{{ item.degree }}</li>
+              <li>{{ item.gpa }}</li>
+            </ul>
           </v-card>
         </v-flex>
       </v-layout>
@@ -29,3 +32,40 @@ export default {
   })
 };
 </script>
+
+<style lang="scss" scoped>
+#inspire.v-application.theme--dark {
+  .v-content {
+    .v-card {
+      border: none;
+      p {
+        width: 100%;
+        line-height: 1.3em;
+        margin-bottom: 0.5rem; /* between paragraphs */
+      }
+      p.title {
+        border-radius: 8px;
+        margin: 0.75rem;
+        padding: 1rem;
+        background-color: rgba(55, 89, 129, 0.432);
+      }
+      ul {
+        line-height: 1.3em;
+        margin-top: 0.5rem; /* between paragraphs */
+        border-radius: 8px;
+        padding: 0.75rem;
+        background-color: rgba(33, 56, 82, 0.637);
+        li {
+          margin-left: 1rem;
+        }
+      }
+      background-image: linear-gradient(
+        to bottom,
+        rgb(157, 197, 221),
+        rgb(112, 158, 187)
+      );
+    }
+  }
+}
+</style>
+
--- vuecli-vuetify-cd/src/views/WorkHistory.vue
+++ vuecli-vuetify-cd/src/views/WorkHistory.vue
@@ -5,11 +5,100 @@
         <span class="display-2">Work History</span>
       </v-layout>
     </v-container>
+    <v-container fluid>
+      <v-layout wrap align-end justify-center row fill-height>
+        <v-flex xs10 sm8 md5 ma-2 pa-2 v-for="(item, i) in history" :key="i">
+          <v-card
+            class="pa-1"
+            hover
+            @click="item.accomplishmentsDisplayed = !item.accomplishmentsDisplayed"
+          >
+            <v-layout align-center justify-center>
+              <p class="title text-center title-border">{{ item.name }}</p>
+            </v-layout>
+            <v-divider></v-divider>
+            <ul class="subtitle-2 list-border">
+              <li>{{ item.title }}</li>
+              <li>{{ item.duration }}</li>
+            </ul>
+            <v-btn
+              v-show="item.accomplishments.length > 0"
+              absolute
+              color="orange"
+              class="white--text"
+              fab
+              small
+              right
+              bottom
+            >
+              <v-icon v-if="item.accomplishmentsDisplayed">mdi-chevron-down</v-icon>
+              <v-icon v-else>mdi-chevron-up</v-icon>
+            </v-btn>
+            <v-expand-transition>
+              <div v-show="item.accomplishmentsDisplayed && (item.accomplishments.length > 0)">
+                <ul class="ma-1 pa-1">
+                  <li
+                    v-for="(accomplishment, j) in item.accomplishments"
+                    :key="j"
+                    class="subtitle-2"
+                  >{{ accomplishment}}</li>
+                </ul>
+              </div>
+            </v-expand-transition>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
+import json from "../assets/work-data.json";
 export default {
-  data: () => ({})
+  data: () => ({
+    history: json
+  })
 };
 </script>
+
+<style lang="scss" scoped>
+.title-border {
+  width: 100%;
+  line-height: 1.3em;
+  margin-bottom: 0.5rem; /* between paragraphs */
+  border-radius: 6px 6px 12px 12px;
+  background-color: rgba(107, 176, 255, 0.151);
+}
+.list-border {
+  border-radius: 6px 6px 12px 12px;
+  background-color: rgba(33, 56, 82, 0.637);
+  margin: 0.5rem; /* between paragraphs */
+  padding: 0.75rem;
+  ul {
+    line-height: 1.3em;
+  }
+}
+#inspire.v-application.theme--dark {
+  .v-content {
+    .v-card {
+      border: none;
+      border-radius: 8px 8px 20px 20px;
+      p.title {
+        margin: 0.75rem;
+        padding: 1rem;
+      }
+      ul {
+        li {
+          list-style-position: inside;
+        }
+      }
+      background-image: linear-gradient(
+        to bottom,
+        rgb(36, 99, 138),
+        rgb(26, 69, 95)
+      );
+    }
+  }
+}
+</style>
+
